779a77abbd40720a124ae9436441817f3bd54ab3
Isabella Stevani <bellastevani@gmail.com>
2019-03-26 22:08:38 -0300

    Split images folder into discrete and continuous. Add uncertain simulation to both discrete and continuous cases.

--- main_d.m
+++ main_d.m
@@ -5,7 +5,7 @@
 % Polytechnic School of The University of Sao Paulo, Dept. of 
 % Telecommunications and Control (PTC)
 % E-mail address: isabella.stevani@usp.br
-% Creation: Aug 2018; Last revision: 18-Mar-2019
+% Creation: Aug 2018; Last revision: 26-Mar-2019
 
 close all; clear; clc;
 
@@ -110,8 +110,8 @@ for i = 1:numel(lines)
   lines(i).LineWidth = 2;
 end
 set(gcf,'PaperPositionMode','auto');
-saveas(gcf,['images/cntr_nomsim_states','.eps'], 'epsc');
-saveas(gcf,'images/cntr_nomsim_states.png');
+saveas(gcf,['images/discrete/cntr_nomsim_states','.eps'], 'epsc');
+saveas(gcf,'images/discrete/cntr_nomsim_states.png');
 % 
 % Control signal
 figure; leg = {'FL'};
@@ -135,8 +135,8 @@ for i = 1:numel(lines)
 end
 supertitle(tlab,18);
 set(gcf,'PaperPositionMode','auto');
-saveas(gcf,['images/cntr_nomsim_cntrsig','.eps'], 'epsc');
-saveas(gcf,'images/cntr_nomsim_cntrsig.png');
+saveas(gcf,['images/discrete/cntr_nomsim_cntrsig','.eps'], 'epsc');
+saveas(gcf,'images/discrete/cntr_nomsim_cntrsig.png');
 
 % Actuated states errors
 r = ref(1:2,:);
@@ -162,7 +162,135 @@ for i = 1:numel(lines)
 end
 supertitle(tlab,18);
 set(gcf,'PaperPositionMode','auto');
-saveas(gcf,['images/cntr_nomsim_error','.eps'], 'epsc');
-saveas(gcf,'images/cntr_nomsim_error.png');
+saveas(gcf,['images/discrete/cntr_nomsim_error','.eps'], 'epsc');
+saveas(gcf,'images/discrete/cntr_nomsim_error.png');
+
+%% Uncertain simulation
+
+n = 10; %samples
+tolI = 0.2; % 20% accurate (inertia parameters)
+toll = 0.01; % 1% accurate (bar lengths)
+p = 4;
+
+fig_x = figure;
+fig_u = figure;
+fig_e = figure;
+
+for i = 1:n
+    
+    uncparam.g = g;
+    uncparam.m1 = round(m1*(1+tolI*(-1+rand(1)*2)),p);
+    uncparam.m2 = round(m2*(1+tolI*(-1+rand(1)*2)),p);
+    uncparam.l0 = round(l0*(1+toll*(-1+rand(1)*2)),p);
+    uncparam.l1 = round(l1*(1+toll*(-1+rand(1)*2)),p);
+    uncparam.l2 = round(l2*(1+toll*(-1+rand(1)*2)),p);
+    uncparam.lg1 = round(lg1*(1+tolI*(-1+rand(1)*2)),p);
+    uncparam.lg2 = round(lg2*(1+tolI*(-1+rand(1)*2)),p);
+    uncparam.Jz1 = round(Jz1*(1+tolI*(-1+rand(1)*2)),p);
+    uncparam.Jz2 = round(Jz2*(1+tolI*(-1+rand(1)*2)),p);
+    
+    %%% Dynamics
+    [q,dq,u] = ParallelRobDynamics_d(x0,t,param,uncparam,lambda,ref,... 
+        cord,T,sat);
+
+    % States
+    figure(fig_x);
+    for i = 1:6
+        subplot(3,2,i); hold on;
+        plot(t,q(i,:),'-b');
+    end
+    
+    % Control signal
+    figure(fig_u);
+    for i = 1:2
+        subplot(2,1,i); hold on;
+        plot(t,u(i,:),'-b');
+    end
+
+    % Actuated states errors
+    r = ref(1:2,:);
+    e = r-[q(3,:);q(5,:)];
+    figure(fig_e);
+    for i = 1:2
+        subplot(2,1,i); hold on;
+        plot(t,e(i,:),'-b');
+    end
+    
+end
+
+% Graphics
+
+%%% States
+leg = {'FL'};
+ylab = {'x [m]','y [m]','t11 [rad]','t12 [rad]','t21 [rad]','t22 [rad]'};
+xlab = 'Time [s]';
+tlab = 'States';
+figure(fig_x); hold off;
+for i = 1:6
+%     lgd = legend(leg);
+    xl = xlabel(xlab);
+    yl = ylabel(ylab{i});
+    set(xl,'FontSize',16);
+    set(yl,'FontSize',16);
+%     set(lgd,'FontSize',12);
+end
+supertitle(tlab,18);
+lines = findobj(gcf,'Type','Line');
+for i = 1:numel(lines)
+    lines(i).LineWidth = 2;
+end
+set(gcf,'PaperPositionMode','auto');
+saveas(gcf,['images/discrete/cntr_uncsim_states','.eps'], 'epsc');
+saveas(gcf,'images/discrete/cntr_uncsim_states.png');
+
+%%% Control signal
+leg = {'FL'};
+xlab = {'','Time [s]'};
+ylab = {'u1 [N.m]','u2 [N.m]'};
+tlab = 'Control Signals';
+figure(fig_u); hold off;
+for i = 1:2
+    subplot(2,1,i)
+    grid on;
+%     lgd = legend(leg);
+    xl = xlabel(xlab{i});
+    yl = ylabel(ylab{i});
+%     set(lgd,'FontSize',12);
+    set(xl,'FontSize',16);
+    set(yl,'FontSize',16);
+end
+lines = findobj(gcf,'Type','Line');
+for i = 1:numel(lines)
+    lines(i).LineWidth = 2;
+end
+supertitle(tlab,18);
+set(gcf,'PaperPositionMode','auto');
+saveas(gcf,['images/discrete/cntr_uncsim_cntrsig','.eps'], 'epsc');
+saveas(gcf,'images/discrete/cntr_uncsim_cntrsig.png');
+
+%%% Actuated states errors
+leg = {'FL'};
+xlab = {'','Time [s]'};
+ylab = {'e1 [rad]','e2 [rad]'};
+tlab = 'Actuated Angle Errors';
+figure(fig_e); hold off;
+for i = 1:2
+    subplot(2,1,i)
+    grid on;
+%     lgd = legend(leg);
+    xl = xlabel(xlab{i});
+    yl = ylabel(ylab{i});
+%     set(lgd,'FontSize',12);
+    set(xl,'FontSize',16);
+    set(yl,'FontSize',16);
+end
+lines = findobj(gcf,'Type','Line');
+for i = 1:numel(lines)
+    lines(i).LineWidth = 2;
+end
+supertitle(tlab,18);
+set(gcf,'PaperPositionMode','auto');
+saveas(gcf,['images/discrete/cntr_uncsim_error','.eps'], 'epsc');
+saveas(gcf,'images/discrete/cntr_uncsim_error.png');
 
 %------------- END OF CODE --------------
