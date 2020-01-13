5a2388258c796bb9d468d9dc63019a8bd40aeae4
Isabella Stevani <bellastevani@gmail.com>
2020-01-13 15:42:56 -0300

    Change frequency of the reference signal

--- reference/RoundInput.m
+++ reference/RoundInput.m
@@ -1,6 +1,6 @@
 function [q0,dq0,d2q0] = RoundInput(t)
 % Generates 2x1 circular signal with center at (0,0.17), 0.07 radius and
-% 10 rad/s frequency. Its derivatives are computed as well.
+% 1 Hz frequency. Its derivatives are computed as well.
 % Inputs:
 %   t: time vector
 % Outputs:
@@ -8,7 +8,7 @@ function [q0,dq0,d2q0] = RoundInput(t)
 %   dq: first derivative [2x1]
 %   d2q: second derivative [2x1]
 
-    C = [0;0.17]; R = 0.07; w = 10.0;
+    C = [0;0.17]; R = 0.07; f = 1; w = 2*pi*f;
 
     q0 = [C(1) + R*cos(w*t);
           C(2) + R*sin(w*t)];
